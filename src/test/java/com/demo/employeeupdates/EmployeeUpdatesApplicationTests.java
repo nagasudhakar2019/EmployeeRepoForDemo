ade918d1037c3b583c9ac061523d7d627adb17a1
Nagasudhakar Patamsetty <npatamse@npatamse-mn2.linkedin.biz>
2021-10-01 15:03:37 +0530

    changes

--- src/test/java/com/demo/employeeupdates/EmployeeUpdatesApplicationTests.java
+++ src/test/java/com/demo/employeeupdates/EmployeeUpdatesApplicationTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
